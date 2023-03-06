0ee6c5f6a1747c55b0a82d59de736e6058eaaeef
A21RoiGF <a21roigf@iessanclemente.net>
2023-03-06 13:23:41 +0100

    v1.11

--- models/order.py
+++ models/order.py
@@ -10,7 +10,7 @@ class OrderLine(models.Model):
     _name = 'order.line'
     _description = 'Linea de pedido'
 
-    # many2one pq una linea solo puede tener un producto pero un producto puede estar en muchas lineas
+    # many2one pq un producto puede estar en varias lineas y ???
     product_id=fields.Many2one('product',string='Productos')
     amount=fields.Integer('Cantidad') 
 
@@ -19,17 +19,17 @@ class Order(models.Model):
     _name = 'order'
     _description = 'Pedido'
 
-    programmed_date = fields.Date('Fecha comienzo')
+    programmed_date = fields.Date('Fecha comienzo',required=True)
     
-    # many2many pq un pedido puede tener varios productos y un producto estar en varios pedidos
-    order_lines=fields.Many2many('order.line',string='Lineas del pedido')
+    # many2many pq un pedido puede tener varias lineas y estar en otros pedidos
+    order_lines=fields.Many2many('order.line',string='Lineas del pedido',required=True)
 
-    frecuency = fields.Integer('Repartir cada')
+    frecuency = fields.Integer('Repartir cada',required=True)
     frecuency_states = fields.Selection([
         ('daily', 'Dias'),
         ('monthly', 'Meses'),
         ('weekly', 'Semanas')],
-        'Espacio de tiempo', default="weekly")
+        'Espacio de tiempo', default="weekly",required=True)
     
     next_delivery_date=fields.Date('Fecha entrega',compute='calculate_delivery_date',readonly=True)
 
@@ -42,11 +42,11 @@ class Order(models.Model):
             elif(order.frecuency_states=='weekly'):
                 order.next_delivery_date=order.programmed_date+timedelta(weeks=order.frecuency)
 
-    total_price=fields.Float('Precio total')
-    #total_price=fields.Float('Precio total',compute='calculate_total_price',readonly=True)
+    total_price=fields.Float('Precio total',compute='calculate_total_price',readonly=True)
 
-#    def calculate_total_price(self):
-#        for order in self:
-#            totalPrice=0
-#            for order_line in order.order_line_ids:
-#                totalPrice+=order_line.product_id.current_price*order_line.amount
+    def calculate_total_price(self):
+        for order in self:
+            totalPrice=0
+            for order_line in order.order_lines:
+                totalPrice+=order_line.product_id.current_price*order_line.amount
+            order.total_price+=totalPrice
